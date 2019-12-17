ec9fd9c1d8d7b99f73121e9dd9b89291ea54212c
syq <syq18837247553@163.com>
2019-12-17 20:31:37 +0800

    sout 123

--- src/main/java/com/example/gitproject/GitprojectApplication.java
+++ src/main/java/com/example/gitproject/GitprojectApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitprojectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GitprojectApplication.class, args);
+        System.out.println("123");
+    }
 
 }
