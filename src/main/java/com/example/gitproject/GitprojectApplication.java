1931409ae36a50ac72c3c43edf8143848ee7b7d0
syq <syq18837247553@163.com>
2019-12-17 21:12:33 +0800

    idea update

--- src/main/java/com/example/gitproject/GitprojectApplication.java
+++ src/main/java/com/example/gitproject/GitprojectApplication.java
@@ -15,6 +15,7 @@ public class GitprojectApplication {
         System.out.println("dev2");
         System.out.println("");
         System.out.println("Githum 修改");
+        System.out.println("IDEA 修改");
     }
 
 }
