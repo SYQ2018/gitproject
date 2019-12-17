730f795a63cca6ad9cd5d1f873be96fbeb4ac0d3
syq <syq18837247553@163.com>
2019-12-17 20:48:25 +0800

    dev2

--- src/main/java/com/example/gitproject/GitprojectApplication.java
+++ src/main/java/com/example/gitproject/GitprojectApplication.java
@@ -12,6 +12,7 @@ public class GitprojectApplication {
         System.out.println("dev");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("dev2");
     }
 
 }
